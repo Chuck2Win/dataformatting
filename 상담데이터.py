b5eaa59f680154196cc4176c0aa8677c48b87fd8
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-09-15 17:18:36 +0900

    Update 상담데이터.py

--- 상담데이터.py
+++ 상담데이터.py
@@ -4,11 +4,13 @@ from tqdm import tqdm
 input_dir=r'/data/nlp/ai_data/NIA/상담_데이터'
 output_dir = r'/data/nlp/ai_data/ok_0915/NIA/상담_데이터'
 os.makedirs(output_dir, exist_ok=True)
-data_list = [i for i in os.listdir(input_dir) if os.path.isdir(i)]
+data_list = ['data','data2','data3','data4','data5']
 for _,i in enumerate(data_list):
     F = open(os.path.join(output_dir,'상담_데이터_%d.txt'%(_+1)),'w',encoding = 'utf-8')
-    for j in tqdm(i):
-        new_path = os.path.join(input_dir, i, j)
+    out_path = os.path.join(input_dir, i)
+    out_list = os.listdir(out_path)
+    for j in tqdm(out_list):
+        new_path = os.path.join(out_path, j)
         inner_path = [i for i in os.listdir(new_path) if i.endswith('txt')]
         new_path = os.path.join(new_path, inner_path[0])
         data = open(new_path,'r', encoding = 'utf-8').read()
